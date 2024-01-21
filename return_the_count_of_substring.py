115b28661a10017361ea256d512bd15c8297f3bb
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-22 07:28:58 +0800

    pseudocode

--- return_the_count_of_substring.py
+++ return_the_count_of_substring.py
@@ -1,2 +1,8 @@
 # Exercise 7: Return the count of a given substring from a string
 # Write a program to find how many times substring “Emma” appears in the given string.
+
+# Pseudocode
+
+# Create a variable for given string
+# Use .count() to count "Emma" 
+# Print the results 
